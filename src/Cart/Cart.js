1f4301f758e71a75bd376fb2ed8363332f401f1a
MirzaAyon <ayonmirza98@gmail.com>
2022-08-14 09:25:08 +0600

    random number and randon product is showing

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -3,6 +3,17 @@ import "./Cart.css";
 import { IoTrashBin } from "react-icons/io5";
 
 const Cart = ({ cart, products, handleClearCart }) => {
+  console.log(products) //inspact e ekta array showing
+  const [freeProduct, setFreeProduct] = useState({})
+  //single product obj er modhe pabo tai empty obj dilam 
+  const item = products[5];
+  console.log(item);
+
+  const handleOffer = () => {
+    const randomNumber = Math.floor((Math.random() * products.length));
+    const item = products[randomNumber]
+    console.log(item);
+  }
   return (
     <div className='cart'>
       <div className='cart-header'>
@@ -26,6 +37,9 @@ const Cart = ({ cart, products, handleClearCart }) => {
           </div>
         </div>
       ))}
+      <p><marquee behavior="" direction="">Buy one get one free</marquee></p>
+      <button className="offer-button" onClick={handleOffer}>Get one for me</button>
+      {/* ekhane kno pm pass kora lgbe na  */}
     </div>
   );
 };
